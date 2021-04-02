bbc681278823ed557dc0f997a9c54e0a823d2ab9
Alvaro Bartolome <alvarob96@usal.es>
2021-04-02 12:22:49 +0200

    resnet architecture support cifar/imagenet proposals & add mxnet req

--- implementation/resnet.py
+++ implementation/resnet.py
@@ -42,28 +42,25 @@ class ResNet(nn.Module):
     def __init__(self, blocks: List[int], filters: List[int], num_classes: int) -> None:
         super(ResNet, self).__init__()
 
-        assert len(blocks) == 3, "ResNet for CIFAR10 uses just 3 residual layers (ImageNet uses 4)"
+        assert len(blocks) == 3 or len(blocks) == 4, "ResNet can either have `3` blocks for CIFAR10, or `4` for ImageNet"
         assert len(blocks) == len(filters), "# of blocks must match # of filters"
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=filters[0], kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=filters[0])
         
-        # Input image shape is 32x32 (no subsampling/downsampling)
         self.rl1 = self._make_layers(num_blocks=blocks[0], planes=filters[0], subsampling=False)
-
-        # Input image shape is 32x32 (subsampling/downsampling in the first layer of the block, 32x32 -> 16x16)
         self.rl2 = self._make_layers(num_blocks=blocks[1], planes=filters[0], subsampling=True)
-
-        # Input image shape is 16x16 (subsampling/downsampling in the first layer of the block, 16x16 -> 8x8)
         self.rl3 = self._make_layers(num_blocks=blocks[2], planes=filters[1], subsampling=True)
+        if len(blocks) == 4: self.rl4 = self._make_layers(num_blocks=blocks[3], planes=filters[2], subsampling=True)
         
-        self.fc1 = nn.Linear(filters[2], num_classes)
+        self.fc1 = nn.Linear(filters[-1], num_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.rl1(x)
         x = self.rl2(x)
         x = self.rl3(x)
+        if hasattr(self, 'rl4'): x = self.rl4(x)
         x = F.avg_pool2d(x, kernel_size=x.size()[3])
         x = x.view(x.size(0), -1)
         x = F.log_softmax(self.fc1(x), dim=1)
@@ -85,12 +82,29 @@ class ResNet(nn.Module):
  
 
 if __name__ == "__main__":
+    # CIFAR10 ResNet20
     net = ResNet(blocks=[3, 3, 3], filters=[16, 32, 64], num_classes=10)
     print(net)
     
     import torch
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
     
     x = torch.randn((1, 3, 32, 32))
+    y = net(x)
+    
+    print(x.shape, y.shape)
+    print(sum(param.numel() for param in net.parameters() if param.requires_grad))
+
+    # ImageNet ResNet18
+    net = ResNet(blocks=[2, 2, 2, 2], filters=[64, 128, 256, 512], num_classes=1000)
+    print(net)
+    
+    import torch
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    
+    x = torch.randn((1, 3, 224, 224))
     y = net(x)
     
     print(x.shape, y.shape)
