a9c19581cd421e97a5641fe50aaf8aacf3b2fada
Alvaro Bartolome <alvarob96@usal.es>
2021-04-04 12:50:09 +0200

    removed flax source code, included jax reference & postponed implementation

--- resnet-jax/resnet.py
+++ resnet-jax/resnet.py
@@ -1,33 +1,7 @@
-"""ResNet Implementation in PyTorch
+"""ResNet Implementation in JAX
 
 He, Kaiming, et al. 'Deep Residual Learning for Image Recognition'
 https://arxiv.org/pdf/1512.03385.pdf
 """
 
-from flax import linen as nn
-
-
-# https://github.com/google/flax/blob/master/examples/imagenet/models.py
-class ResNetBlock(nn.Module):
-  """ResNet block."""
-  filters: int
-  conv: ModuleDef
-  norm: ModuleDef
-  act: Callable
-  strides: Tuple[int, int] = (1, 1)
-
-  @nn.compact
-  def __call__(self, x,):
-    residual = x
-    y = self.conv(self.filters, (3, 3), self.strides)(x)
-    y = self.norm()(y)
-    y = self.act(y)
-    y = self.conv(self.filters, (3, 3))(y)
-    y = self.norm(scale_init=nn.initializers.zeros)(y)
-
-    if residual.shape != y.shape:
-      residual = self.conv(self.filters, (1, 1),
-                           self.strides, name='conv_proj')(residual)
-      residual = self.norm(name='norm_proj')(residual)
-
-    return self.act(residual + y)
+# Reference: https://github.com/google/jax/blob/master/examples/resnet50.py
