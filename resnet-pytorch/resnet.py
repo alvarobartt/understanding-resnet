fixed training (wrong AverageMeter placement) & temp remove of AdaptiveAvgPool2d

resnet-pytorch/resnet.py
@@ -104,7 +104,7 @@ class ResNet(nn.Module):
         if len(blocks) == 4: self.rl4 = self._make_layer(block=block, num_blocks=blocks[3], planes=filters[3], stride=2)
 
         self.in_features = filters[-1] * block.expansion
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+        # self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
         self.fc = nn.Linear(in_features=self.in_features, out_features=num_classes)
 
         # https://discuss.pytorch.org/t/how-are-layer-weights-and-biases-initialized-by-default/13073/4
@@ -117,8 +117,10 @@ class ResNet(nn.Module):
         x = self.rl2(x)
         x = self.rl3(x)
         if hasattr(self, 'rl4'): x = self.rl4(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        # x = self.avgpool(x)
+        # x = torch.flatten(x, 1)
+        x = F.avg_pool2d(x, x.size()[3])
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
 

resnet-pytorch/train.py
@@ -35,7 +35,12 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
     device = torch.device(select_device())
 
     # Initiliaze ResNet for CIFAR10 and move it to the GPU (CPU if not available)
+    model = nn.DataParallel(model)
     model.to(device)
+
+    # https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
+    # https://pytorch.org/docs/stable/backends.html
+    if torch.backends.cudnn.is_available(): torch.backends.cudnn.benchmark = True
     
     # Count the total number of trainable parameters
     trainable_parameters = count_trainable_parameters(model=model)
@@ -80,6 +85,7 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
 
     # Define the loss function, the optimizer, and the learning rate scheduler
     criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(device)
     optimizer = optim.SGD(model.parameters(), lr=1e-1, momentum=9e-1, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=LR_MILESTONES, gamma=1e-1)
 
@@ -92,6 +98,8 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
     config.iterations = ITERATIONS
     config.epochs = EPOCHS
     config.batch_size = BATCH_SIZE
+    config.batches_train = len(train_dataloader)
+    config.batches_test = len(test_dataloader)
     config.architecture = model_name
     config.dataset = 'cifar10'
     config.dataset_train_size = len(train_dataset)
@@ -104,18 +112,16 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
     config.learning_rate = 1e-1
     config.learning_rate_milestones = LR_MILESTONES
 
-    # Initialize AverageMeters before training
-    train_losses = AverageMeter()
-    train_top1 = AverageMeter()
-    test_losses = AverageMeter()
-    test_top1 = AverageMeter()
-
-    # Define best TOP-1 accuracy
-    best_top1 = 0.
+    # Define best prec@1 before training
+    best_prec1 = 0.
 
     # Training loop with wandb logging
     wandb.watch(model)
     for epoch in range(1, EPOCHS+1):
+        # Initialize AverageMeters before training
+        train_losses = AverageMeter()
+        train_prec1 = AverageMeter()
+
         model.train()
         start_time = time()
 
@@ -131,12 +137,12 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
 
             outputs, loss = outputs.float(), loss.float()
 
-            top1 = accuracy(outputs.data, labels)[0]
+            prec1 = accuracy(outputs.data, labels)[0]
             train_losses.update(loss.item(), inputs.size(0))
-            train_top1.update(top1.item(), inputs.size(0))
+            train_prec1.update(prec1.item(), inputs.size(0))
         
         train_loss = train_losses.avg
-        train_acc = train_top1.avg / 100
+        train_acc = train_prec1.avg / 100
         train_error = 1.0 - train_acc
         train_time = time() - start_time
 
@@ -146,6 +152,10 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
         }, step=epoch)
 
         scheduler.step()
+
+        # Initialize AverageMeters before evaluation
+        test_losses = AverageMeter()
+        test_prec1 = AverageMeter()
 
         model.eval()
         start_time = time()
@@ -159,12 +169,12 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
 
                 outputs, loss = outputs.float(), loss.float()
 
-                top1 = accuracy(outputs.data, labels)[0]
+                prec1 = accuracy(outputs.data, labels)[0]
                 test_losses.update(loss.item(), inputs.size(0))
-                test_top1.update(top1.item(), inputs.size(0))
+                test_prec1.update(prec1.item(), inputs.size(0))
             
             test_loss = test_losses.avg
-            test_acc = test_top1.avg / 100
+            test_acc = test_prec1.avg / 100
             test_error = 1.0 - test_acc
             test_time = time() - start_time
             
@@ -173,10 +183,10 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
                 'test_error': test_error, 'test_time': test_time
             }, step=epoch)
 
-        if best_top1 is None: best_top1 = test_acc
-        if best_top1 <= test_acc:
+        if best_prec1 is None: best_prec1 = test_acc
+        if best_prec1 <= test_acc:
             torch.save(model.state_dict(), os.path.join(wandb.run.dir, f"{model_name}-cifar10.pth"))
-            best_top1 = test_acc
+            best_prec1 = test_acc
 
     # Finish logging this wandb run
     # https://docs.wandb.ai/library/init#how-do-i-launch-multiple-runs-from-one-script
@@ -184,8 +194,4 @@ def train_resnet_cifar10(model: ResNet, model_name: str) -> None:
 
 
 if __name__ == '__main__':
-    # https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
-    # https://pytorch.org/docs/stable/backends.html
-    if torch.backends.cudnn.is_available(): torch.backends.cudnn.benchmark = True
-
     train_resnet_cifar10(model=resnet20(), model_name='resnet20')